c2f4ed4a639760ad47a18313dc6266cdb905974c
Ghaith200 <abdullahghiath2003@gmail.com>
2026-04-11 01:51:07 +0200

    sss

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
-﻿"use client";
-
-import { useState } from "react";
+﻿import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { motion, Variants, AnimatePresence } from "framer-motion";
