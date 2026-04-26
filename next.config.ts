f7953664f49fc59af789b317fcec05126c629839
amrd12 <amr.elnbawy220@gmail.com>
2026-04-27 01:19:23 +0300

    fix build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', // This tells Next.js to build a static "out" folder
   images: {
     unoptimized: true, // Disable image optimization for static export
   },
